0d42ffc3ac6e230ae9458f3314649774073a2a46
bariflor <barihuamachuco@gmail.com>
2017-11-15 15:08:03 -0500

    añadi header, content1,2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+   $('.carousel').carousel()
 });
