1de2115aa022bbba3e7a17dbaa7a1ea4cad71082
bariflor <barihuamachuco@gmail.com>
2017-11-20 16:20:01 -0500

    añadi cambios-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
    $('.carousel').carousel()
- 
+});
+$(window).scroll(function() {
+        if ($("#menu").offset().top > 56) {
+            $("#menu").addClass("bg-inverse");
+        } else {
+            $("#menu").removeClass("bg-inverse");
+        }
 });
